723738036c6fb27ed2b5f9cbc40fb2aa284fa4d9
Carlos Salazar <carlosiusalazar@gmail.com>
2020-08-21 20:01:24 +0900

    Flask basic site and Jinja2 Template

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    #return render_template('index.html')
-    return redirect(url_for('about'))
+    fruits = ['Apple', 'Mango','Orange']
+    return render_template('index.html', fruits=fruits)
+    #return redirect(url_for('about'))
 
 @app.route('/about')
 def about():
@@ -13,3 +14,25 @@ def about():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
+# <!DOCTYPE html>
+# <html>
+#   <head>
+#     <title>My Project</title>
+#   </head>
+#   <body>
+#     <ul>
+#       {% for fruit in fruits %}
+#         <li>{{fruit}}</li>
+#       {% endfor %}
+#     </ul>
+#     {% if 'Mango' in fruits %}
+#       <p>Mango exists in the list</p>
+#     {% else %}
+#       <p>Mango Desn't exist in the list</p>
+#     {% endif %}
+#   </body>
+# </html>
