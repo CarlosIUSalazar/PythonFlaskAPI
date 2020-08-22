c1fe88355ab9d5f63c7fd6f76db75d6b34caf9ba
Carlos Salazar <carlosiusalazar@gmail.com>
2020-08-22 16:19:03 +0900

    Installed MySQL, added Create and Read logic

--- app.py
+++ app.py
@@ -1,23 +1,45 @@
 from flask import Flask, render_template, url_for, redirect
+from flask_bootstrap import Bootstrap
+from flask_mysqldb import MySQL
+import yaml
+
 
 app = Flask(__name__)
+Bootstrap(app)
+
+#Configure db
+db = yaml.load(open('db.yaml'))
+app.config['MYSQL_HOST'] = db['mysql_host']
+app.config['MYSQL_USER'] = db['mysql_user']
+app.config['MYSQL_PASSWORD'] = db['mysql_password']
+app.config['MYSQL_DB'] = db['mysql_db']
+mysql = MySQL(app)
 
 @app.route('/')
 def index():
-    fruits = ['Apple', 'Mango','Orange']
-    return render_template('index.html', fruits=fruits)
+    cur = mysql.connection.cursor()
+    cur.execute("INSERT INTO user VALUES(%s)",['Mike'])
+    mysql.connection.commit()   #saves the changes into db
+    return render_template('index.html')
+
+    #fruits = ['Apple', 'Mango','Orange']
+    #return render_template('index.html', fruits=fruits)
     #return redirect(url_for('about'))
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
+@app.route('/css')
+def css():
+    return render_template('css.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
 
 
 
-
+#From index.html
 # <!DOCTYPE html>
 # <html>
 #   <head>
